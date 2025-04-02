8c27cd86d8ab3d95144d3b05d6922d920c4cafd4
Theivaraj-dev <theivaraj777@gmail.com>
2025-04-02 23:23:34 +0530

    ecommerce project

--- myapp/settings.py
+++ myapp/settings.py
@@ -74,17 +74,17 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'ecommerce',
-        'USER': 'root',
-        'PASSWORD':'12345',
-        'HOST':'localhost',
-        'PORT':'3306',
-       
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME':'ecommerce',
+#        'USER': 'root',
+#        'PASSWORD':'12345',
+#        'HOST':'localhost',
+#        'PORT':'3306',
+#       
+#    }
+#}
 
 
 # Password validation
